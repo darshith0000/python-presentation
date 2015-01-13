dae23014f35ecf39516135492d92531859e359b7
Abhishek-jaiswal <ia.malhotra@gmail.com>
2015-01-13 16:57:41 +0530

    initial commit

---
+++ slide_config.js
@@ -0,0 +1,42 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Getting started with Python',
+    subtitle: "Installation & it's various datatypes",
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Ansuman Bebarta & Abhisekh Jaiswal',
+    company: 'Team Members | Bhubaneswar Pythonists',
+    //meetup: 'http://bit.ly/1xcTaJW',
+    //facebook: 'http://www.facebook.com/bhubaneswarpythonists'
+    //gplus: 'http://bit.ly/1xcTaJW',
+    //twitter: '@bbsrpython',
+    //youtube: 'http://bit.ly/1tSxDT8',
+    github: 'http://github.com/you'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
